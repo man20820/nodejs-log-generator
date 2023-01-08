1a3e121ac593510081c15ab4cb51150b4661eca6
man20820 <fatkhurrahmandjogja@gmail.com>
2023-01-09 03:43:50 +0700

    add dynamic config via curl

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 var cron = require('node-cron');
 
-cron.schedule('*/3 * * * * *', () => {
-  console.log('ini adalah pesan log pertama');
+app.use(cors());
+
+const port = process.env.PORT || $PORT;
+
+app.route("/").get((req, res) => {
+  res.send("info man@tkjpedia.com");
 });
 
-cron.schedule('*/3 * * * * *', () => {
-  console.log('ini adalah pesan log kedua');
+let task = null
+
+app.route("/log/:message/:frequency").get((req, res) => {
+  let message = req.params.message
+  let frequency = req.params.frequency
+  if (task !== null) {
+    task.stop()
+  }
+  task = cron.schedule(`*/${ frequency } * * * * *`, () => {
+    console.log(`setiap ${ frequency } detik, ini adalah pesan ${ message } ${ new Date() }`);
+  }, {
+    scheduled: true
+  });
+  res.send(`membuat cron ${ message } setiap ${ frequency } detik sekali`); 
+  console.log(task)
 });
 
-cron.schedule('*/5 * * * * *', () => {
-  console.error('ini adalah pesan error');
-});
+
+
+// cron.schedule('*/3 * * * * *', () => {
+//   console.log('ini adalah pesan log kedua');
+// });
+
+// cron.schedule('*/5 * * * * *', () => {
+//   console.error('ini adalah pesan error');
+// });
+
+app.listen(port);
